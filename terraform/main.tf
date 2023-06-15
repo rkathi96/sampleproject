4a5b03f48e81bee7c0d7cc3ffe3c88c40d275e4a
rkathi96 <81325786+rkathi96@users.noreply.github.com>
2023-06-14 19:45:57 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("path/to/service-account-key.json")
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = var.zone
+
+  # Add any additional instance configurations as needed
+}
+
+output "instance_ip" {
+  value = google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
+}
